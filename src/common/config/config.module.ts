c7d3b0ddc62480e2037667d79e37d9ded17f75ba
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-12 09:24:24 -0500

    feat(config.module.ts): configure 'pino-pretty'

--- src/common/config/config.module.ts
+++ src/common/config/config.module.ts
@@ -2,6 +2,14 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [LoggerModule.forRoot()],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+        },
+      },
+    }),
+  ],
 })
 export class ConfigModule {}
