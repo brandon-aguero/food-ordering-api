f85cabb316376eac4e839630a698213de7409ce7
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-12 15:26:38 -0500

    feat: add configuration for 'ConfigModule' as 'NestConfigModule'

--- src/common/config/config.module.ts
+++ src/common/config/config.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
+import configEnv from './envs/config.env';
+import { validate } from './envs/validation.env';
 
 @Module({
   imports: [
+    NestConfigModule.forRoot({
+      isGlobal: true,
+      load: [configEnv],
+      validate,
+    }),
     LoggerModule.forRoot({
       pinoHttp: {
         transport: {
