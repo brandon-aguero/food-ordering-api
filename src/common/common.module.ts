1a5301f6c1e9cb38f8d02aad7366d672e82c9f39
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-12 08:45:46 -0500

    feat: generate 'config', 'database' and 'swagger' modules

---
+++ src/common/common.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from './config/config.module';
+import { SwaggerModule } from './swagger/swagger.module';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [ConfigModule, SwaggerModule, DatabaseModule],
+})
+export class CommonModule {}
