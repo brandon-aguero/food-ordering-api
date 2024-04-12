5011b0eb75342611b45ed812dfa3b31196c2b41e
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-12 15:41:17 -0500

    feat(config.env.ts): add 'isProd' property to 'config' token

--- src/common/config/envs/config.env.ts
+++ src/common/config/envs/config.env.ts
@@ -3,6 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('config', () => {
   return {
     env: process.env.NODE_ENV,
+    isProd: process.env.NODE_ENV === 'production',
     port: Number(process.env.PORT),
   };
 });
