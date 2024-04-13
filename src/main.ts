8748e76a8160e3893071316052b4f0df24454ed6
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-12 20:17:10 -0500

    feat(main.ts): sort imports and use ConfigService to define the port

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -15,6 +16,7 @@ async function bootstrap() {
     },
   );
   app.useLogger(app.get(Logger));
-  await app.listen(3000, '0.0.0.0');
+  const configService = app.get(ConfigService);
+  await app.listen(Number(configService.get('PORT')), '0.0.0.0');
 }
 bootstrap();
