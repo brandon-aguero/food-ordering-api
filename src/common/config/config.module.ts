b59257624f25b3feb7c48d86510d4f148326eeef
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-12 20:09:41 -0500

    feat: implement CorrelationIdMiddleware and use customProps in pinoHttp

--- src/common/config/config.module.ts
+++ src/common/config/config.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigType, ConfigModule as NestConfigModule } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
+import { FastifyRequest } from 'fastify';
 import configEnv from './envs/config.env';
 import { validate } from './envs/validation.env';
+import {
+  CORRELATION_ID_HEADER,
+  CorrelationIdMiddleware,
+} from './middlewares/correlation-id.middleware';
 
 @Module({
   imports: [
@@ -31,10 +36,19 @@ import { validate } from './envs/validation.env';
               res: () => undefined,
             },
             autoLogging: false,
+            customProps: (req: FastifyRequest['raw']) => {
+              return {
+                correlationId: req.headers[CORRELATION_ID_HEADER],
+              };
+            },
           },
         };
       },
     }),
   ],
 })
-export class ConfigModule {}
+export class ConfigModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorrelationIdMiddleware).forRoutes('*');
+  }
+}
