2791644d832447dde35e98244465d923a99594ec
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-13 13:48:39 -0500

    feat(swagger.module.ts): add 'setup' static method with all configuration

--- src/common/swagger/swagger.module.ts
+++ src/common/swagger/swagger.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import {
+  DocumentBuilder,
+  SwaggerModule as NestSwaggerModule,
+} from '@nestjs/swagger';
+import { RawServerDefault } from 'fastify';
 
 @Module({})
-export class SwaggerModule {}
+export class SwaggerModule {
+  static setup(app: NestFastifyApplication<RawServerDefault>) {
+    const config = new DocumentBuilder()
+      .setTitle('REST API Documentation')
+      .setDescription(
+        'REST API built with NestJS, Fastify, Docker, PostgreSQL and TypeORM',
+      )
+      .setVersion('1.0')
+      .build();
+    const document = NestSwaggerModule.createDocument(app, config);
+    NestSwaggerModule.setup('docs', app, document);
+  }
+}
