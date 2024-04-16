63ae9da087796ad4bb3282aebbec83953ef31903
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-16 09:18:55 -0500

    feat(config.module.ts): remove all configuration from LoggerModule

--- src/common/config/config.module.ts
+++ src/common/config/config.module.ts
@@ -1,13 +1,7 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ConfigType, ConfigModule as NestConfigModule } from '@nestjs/config';
-import { LoggerModule } from 'nestjs-pino';
-import { FastifyRequest } from 'fastify';
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import configEnv from './envs/config.env';
 import { validate } from './envs/validation.env';
-import {
-  CORRELATION_ID_HEADER,
-  CorrelationIdMiddleware,
-} from './middlewares/correlation-id.middleware';
 
 @Module({
   imports: [
@@ -16,39 +10,6 @@ import {
       load: [configEnv],
       validate,
     }),
-    LoggerModule.forRootAsync({
-      inject: [configEnv.KEY],
-      useFactory: (configService: ConfigType<typeof configEnv>) => {
-        const isProd = configService.isProd;
-
-        return {
-          pinoHttp: {
-            transport: !isProd
-              ? {
-                  target: 'pino-pretty',
-                  options: {
-                    colorize: true,
-                  },
-                }
-              : undefined,
-            serializers: {
-              req: () => undefined,
-              res: () => undefined,
-            },
-            autoLogging: false,
-            customProps: (req: FastifyRequest['raw']) => {
-              return {
-                correlationId: req.headers[CORRELATION_ID_HEADER],
-              };
-            },
-          },
-        };
-      },
-    }),
   ],
 })
-export class ConfigModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(CorrelationIdMiddleware).forRoutes('*');
-  }
-}
+export class ConfigModule {}
