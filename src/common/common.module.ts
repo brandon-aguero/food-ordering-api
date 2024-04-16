6d316d143734dcfbcda98b106a70ba45e61b7c2c
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-16 09:12:46 -0500

    feat: generate LoggerModule using NestCLI

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { SwaggerModule } from './swagger/swagger.module';
 import { DatabaseModule } from './database/database.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [ConfigModule, SwaggerModule, DatabaseModule],
+  imports: [ConfigModule, SwaggerModule, DatabaseModule, LoggerModule],
 })
 export class CommonModule {}
