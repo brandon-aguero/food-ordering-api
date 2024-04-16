4f68c62d69cfd60f26862968629f30105b8b65ec
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-16 09:20:50 -0500

    feat(logger.module.ts): add all configuration for LoggerModule from nestjs-pino package

--- src/common/logger/logger.module.ts
+++ src/common/logger/logger.module.ts
@@ -1,4 +1,47 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { LoggerModule as NestLoggerModule } from 'nestjs-pino';
+import { FastifyRequest } from 'fastify';
+import {
+  CORRELATION_ID_HEADER,
+  CorrelationIdMiddleware,
+} from './middlewares/correlation-id.middleware';
+import configEnv from '../config/envs/config.env';
 
-@Module({})
-export class LoggerModule {}
+@Module({
+  imports: [
+    NestLoggerModule.forRootAsync({
+      inject: [configEnv.KEY],
+      useFactory: (configService: ConfigType<typeof configEnv>) => {
+        const isProd = configService.isProd;
+        return {
+          pinoHttp: {
+            transport: !isProd
+              ? {
+                  target: 'pino-pretty',
+                  options: {
+                    colorize: true,
+                  },
+                }
+              : undefined,
+            serializers: {
+              req: () => undefined,
+              res: () => undefined,
+            },
+            autoLogging: false,
+            customProps: (req: FastifyRequest['raw']) => {
+              return {
+                correlationId: req.headers[CORRELATION_ID_HEADER],
+              };
+            },
+          },
+        };
+      },
+    }),
+  ],
+})
+export class LoggerModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorrelationIdMiddleware).forRoutes('*');
+  }
+}
