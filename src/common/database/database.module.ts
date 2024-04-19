64ee6b44fd7e7ec191541f48ac1915a9fb14bafa
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-19 16:54:57 -0500

    feat(database.module.ts): add TypeOrmModule configuration
    configure TypeOrmModule using async configuration with settings from database.config

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import database from '../config/envs/configs/database.config';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [database.KEY],
+      useFactory: (configService: ConfigType<typeof database>) => {
+        const { isProd, postgres } = configService;
+        return {
+          type: 'postgres',
+          url: postgres.url,
+          synchronize: false,
+          autoLoadEntities: true,
+          ssl: isProd
+            ? {
+                rejectUnauthorized: false,
+              }
+            : false,
+        };
+      },
+    }),
+  ],
+})
 export class DatabaseModule {}
