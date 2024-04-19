ce013cddd82399c528ad87ba7a81c1fda3289b45
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-18 19:42:06 -0500

    refactor(main.ts): sort imports

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import {
 } from '@nestjs/platform-fastify';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
-import { AppModule } from './app.module';
 import { SwaggerModule } from './common/swagger/swagger.module';
 import { corsOptions } from './common/config/libs/cors.lib';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
