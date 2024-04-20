ff572990f95dc920463be8043d47f71dc8c77483
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-19 19:38:10 -0500

    feat: use TypeOrmModule.forFeature and import Metadata entity

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Metadata } from '@entities/images/metadata.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Metadata])],
+})
 export class ImagesModule {}
