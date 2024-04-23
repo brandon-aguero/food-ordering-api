37279be2d297e649c3b1c32c0cf963c02636dd9f
Brandon <brandonaguerodeveloper@gmail.com>
2024-04-23 13:18:56 -0500

    feat: generate 'serve-static' module using NestCLI

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,8 +3,15 @@ import { ConfigModule } from './config/config.module';
 import { LoggerModule } from './logger/logger.module';
 import { DatabaseModule } from './database/database.module';
 import { SwaggerModule } from './swagger/swagger.module';
+import { ServeStaticModule } from './serve-static/serve-static.module';
 
 @Module({
-  imports: [ConfigModule, LoggerModule, DatabaseModule, SwaggerModule],
+  imports: [
+    ConfigModule,
+    LoggerModule,
+    DatabaseModule,
+    SwaggerModule,
+    ServeStaticModule,
+  ],
 })
 export class CommonModule {}
